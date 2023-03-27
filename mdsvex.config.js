02c584e97e8ef2e185456db50f78066e2f82a20a
aryadaroui <adaroui@uci.edu>
2023-03-27 06:45:08 -0700

    styled advanced markdown code block features

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -3,35 +3,30 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex-svelte';
 import rehypePrettyCode from 'rehype-pretty-code';
 
-import {unified} from 'unified'
+import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import rehypeStringify from 'rehype-stringify'
 
+
 function highlighter(code, lang, meta) {
-	// console.log("meta", meta);
+	// mdsvex destructure the code block, so we have to reassemble it and pass it to rehype-pretty-code
+
+	// if extra meta is not provided, we need to set an empty string
 	if (meta === null) {
 		meta = "";
 	}
 
-const block = `\`\`\`${lang} ${meta}
+	const block = `\`\`\`${lang} ${meta}
 ${code}
 \`\`\``
 
-
-const file = unified()
-  .use(remarkParse)
-  .use(remarkRehype)
-  .use(rehypePrettyCode, prettyCodeOptions)
-  .use(rehypeStringify)
-  .process(block)
-
-
-//   console.log("file", file);
-
-
-	// console.log("lang", lang);
-	// console.log("meta", meta);
+	const file = unified()
+		.use(remarkParse)
+		.use(remarkRehype)
+		.use(rehypePrettyCode, prettyCodeOptions)
+		.use(rehypeStringify)
+		.process(block)
 
 	return file;
 }
@@ -40,23 +35,23 @@ const prettyCodeOptions = {
 	theme: "rose-pine-moon",
 	keepBackground: true,
 	onVisitLine(node) {
-	  if (node.children.length === 0) {
-		node.children = [
-		  {
-			type: "text",
-			value: " ",
-		  },
-		];
-	  }
+		if (node.children.length === 0) {
+			node.children = [
+				{
+					type: "text",
+					value: " ",
+				},
+			];
+		}
 	},
 	onVisitHighlightedLine(node) {
-	  node.properties.className.push("highlighted");
+		node.properties.className.push("highlighted");
 	},
 	onVisitHighlightedWord(node) {
-	  node.properties.className = ["word"];
+		node.properties.className = ["word"];
 	},
 	tokensMap: {},
-  };
+};
 
 const config = defineConfig({
 	extensions: ['.svelte.md', '.md', '.svx'],
@@ -70,7 +65,7 @@ const config = defineConfig({
 		rehypeKatex
 	],
 
-	highlight: {highlighter},
+	highlight: { highlighter },
 
 });
 
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -40,34 +40,70 @@
 		display: grid;
 	}
 
-	code {
-		background-color: $gray-soft;
-		padding: 0.5rem;
-		border-radius: 0.5rem;}
+	p code {
+		padding: 0.2rem;
+		border-radius: 0.5rem;
+	}
 
-	code {
-
-		// background-color: $background-color;
-		font-family: $mono;
-		font-size: 0.9em;
-
+	div[data-rehype-pretty-code-fragment] {
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+		border-radius: 10px;
 		border: $border-translucent 1px solid;
+		overflow: hidden;
+	}
+
+	div[data-rehype-pretty-code-title] {
+		font-family: $mono;
+		font-size: 0.8em;
+		display: inline-block;
+		width: calc(100% - 1rem);
+		background-color: rgba($background-color, 0.5);
+
+		padding: 0.5em;
+		text-align: right;
+
+		+ pre code {
+			border-radius: 0 0 10px 10px;
+		}
+	}
+
+	code {
+		padding: 0.5rem;
+		border-radius: 10px;
+	}
+
+	pre {
+		margin: 0;
+	}
+
+	code {
+		background-color: rgba($background-color, 0.8);
+		font-family: $mono;
+		font-size: 0.85em;
+
 
 		padding-left: 0;
 		padding-right: 0;
+		overflow: scroll;
 
 		span.highlighted {
 			background-color: rgba($blue-mid, $alpha: 0.2);
 			border-left: $blue-hard 2px solid;
-
 		}
 
+		span.word {
+			background-color: rgba($blue-hard, $alpha: 0.3);
+			padding: 0.4em;
+			border-radius: 0.4em;
+			filter: brightness(1.5);
+		}
 	}
 
 	span.line.highlighted::before {
 		// background-color: $blue-mid;
 		color: white;
-				margin-left: calc(1em - 1px);
+		margin-left: calc(1em - 1px);
 	}
 
 	// line numbers
@@ -88,7 +124,6 @@
 		color: gray;
 
 		margin-left: 1em;
-		
 	}
 
 	code[data-line-numbers-max-digits='2'] > .line::before {
