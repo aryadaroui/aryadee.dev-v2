3eb67bfaba21f8c81840be1f0d36a0d416ef10cf
aryadaroui <adaroui@uci.edu>
2023-04-07 18:34:42 -0700

    proper 404 error handling for blog posts

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,16 +1,28 @@
-import {error} from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
-export async function load({ params }) {
+export async function load({ params, page }) {
 	// error handling for imported file
 	// TODO: check to see if route exists, to throw 404 or 500
-	try {
-		const post = await import(`../../../posts/${params.slug}.svx`);
-		const { title, date, tags } = post.metadata;
-		const content = post.default;
 
-		return { title, date, content, tags };
+	let post;
+	try {
+		post = await import(`../../../posts/${params.slug}.svx`);
 	} catch (e) {
 		console.error(e);
-		throw error(500, 'Internal Server Error');
+		throw error(404, 'Page not found');
+	}
+
+	try {
+		const { title, date, tags } = post.metadata;
+		const content = post.default;
+		return { title, date, content, tags };
+	} catch (e) {
+		// technically this is a server error, but we'll treat it as a 404
+		// because the file exists but doesn't have the right metadata
+		// we'll just log the error to the console
+		console.error("\x1b[31m%s\x1b[0m", "Error loading post metadata:");
+		console.error(e);
+		throw error(404, "Page not found");
+
 	}
 }
