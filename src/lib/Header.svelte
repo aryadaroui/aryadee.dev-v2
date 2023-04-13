3995097bec65ef1dc9c80da321c45312917624c9
aryadaroui <adaroui@uci.edu>
2023-04-13 13:44:59 -0700

    made header home link cuter

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -7,7 +7,7 @@
 <div id="header">
 	<nav>
 		<div>
-			<a href="/" class="link home"> arya<span class="accent">dee</span> </a>
+			<a href="/" class="link home"> <span class="normal">arya</span><span class="accent">dee</span> </a>
 		</div>
 
 		<div>
@@ -25,7 +25,6 @@
 		background-color: $void-color;
 		width: 100%;
 
-
 		nav {
 			margin: auto;
 			display: flex;
@@ -35,16 +34,14 @@
 			max-width: 720px;
 			height: 3em;
 
-
 			@media screen and (max-width: 500px) {
-			flex-direction: column;
-			// height: 4em;
-			gap: 10px;
-			padding-top: 5px;
-			padding-bottom: 20px;
-			// margin-bottom: 0px;
-		}
-
+				flex-direction: column;
+				// height: 4em;
+				gap: 10px;
+				padding-top: 5px;
+				padding-bottom: 20px;
+				// margin-bottom: 0px;
+			}
 
 			a {
 				text-decoration: none;
@@ -90,18 +87,29 @@
 			}
 
 			a:hover {
-				// background-color: rgba($link-background, 0.7);
-				// color: rgba($color: $tan-soft, $alpha: 0.9);
 				color: $cyan-hard;
-				// color: $cyan-soft;
-				// transition: background-color 0.15s ease, color 0.15s ease;
+			}
 
-				// color: $highlight-color;
-				// color: $tan-mid;
-				// text-decoration: underline;
-				// text-underline-offset: 0.2rem;
-				// text-decoration-thickness: 0.05rem;
-				// transition: text-underline-offset 0.1s ease;
+			// a.home :hover {
+			// 	color: $tan-hard;
+			// }
+
+			.normal {
+				transition: color 0.15s ease;
+			}
+
+			.accent {
+				transition: color 0.15s linear;
+			}
+
+			a:hover > .normal {
+				color: $cyan-soft;
+				transition: color 0.15s ease;
+			}
+
+			a:hover > .accent {
+				color: $cyan-hard;
+				transition: color 0.15s linear;
 			}
 
 			a:active {
