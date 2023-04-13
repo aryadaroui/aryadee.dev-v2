7f03b7ecafa0c6a5e7c57c43cd41e5473fbe39d1
aryadaroui <adaroui@uci.edu>
2023-04-13 13:58:44 -0700

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,45 @@
+<script>
+	import Header from '$lib/Header.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import { page } from '$app/stores';
+</script>
+
+<Header />
+<div class="container">
+	<div>
+		<img
+			src="https://i.giphy.com/media/pynZagVcYxVUk/giphy.webp"
+			alt="funny GIF of Michael Scott crying"
+			width="200px"
+		/>
+	</div>
+	<!-- <div class="error-message"> -->
+	<h1>Error: {$page.status}</h1>
+	<!-- </div> -->
+
+	<!-- Object is possibly 'null'.js(2531). This is fine; if isn't null, this page isn't shown.-->
+	<p>{$page.error.message} :-(</p>
+
+</div>
+<Footer />
+
+
+
+<style>
+
+	img {
+		margin: 20px;
+	}
+
+	p {
+		text-align: center;
+	}
+	.container {
+		text-align: center;
+		margin-top: 40px;
+		/* display: flex; */
+		/* flex-direction: column; */
+		/* justify-content: space-between; */
+		height: calc(100vh - 260px);
+	}
+</style>
