3bafca22d536770445f0d0dc8029e51311c8a762
aryadaroui <adaroui@uci.edu>
2023-04-22 16:23:56 -0700

    changed adapter to vercel
    maybe this'll fix deployment err

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-// import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-vercel';
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
 
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
